4cea810aa34ab7a8f5e29f76094984d7c3b61962
김민석 <momoqq@naver.com>
2023-01-17 16:54:40 +0900

    5.2 edit profile

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { authService } from "fbase";
 import { useNavigate } from "react-router-dom";
+import { updateProfile } from "firebase/auth";
 
-const Profile = () => {
+const Profile = ({ refreshUser, userObj }) => {
   const navigate = useNavigate();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
   const onLogOutClick = () => {
     authService.signOut();
     navigate("/");
   };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await updateProfile(authService.currentUser, {
+        displayName: newDisplayName,
+      });
+      refreshUser();
+    }
+  };
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          type="text"
+          placeholder="Display name"
+          value={newDisplayName}
+        />
+        <input type="submit" value="Update Profile" />
+      </form>
       <button onClick={onLogOutClick}>Log Out</button>
     </>
   );
